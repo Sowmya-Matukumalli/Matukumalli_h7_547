46fc3ad7be424c4435044fe51347bee5a65538d9
Sowmya-Matukumalli <32010031+Sowmya-Matukumalli@users.noreply.github.com>
2017-09-16 15:29:51 +0530

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+
+import java.util.Date;
+public class Main {
+
+	public static void main(String[] args) {
+    
+  ArrayList list = new ArrayList();
+
+  list.add(new Student("Sowmya",77.12));
+  list.add(new Student("Srihitha",79.3));
+  list.add(new Student("Ramya",82));
+  list.add(new Student("Prathyusha",77));
+
+  
+		
+		//You may test that your code works find here
+		//Please check that your code works and has no 
+		//compilation problems before to submit
+	}
+
+}
